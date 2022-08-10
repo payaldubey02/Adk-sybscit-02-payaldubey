2726a9e0af3a90db3c58a300b777bf4b62662ab4
payaldubey02 <110089542+payaldubey02@users.noreply.github.com>
2022-08-10 11:36:47 +0530

    Add files via upload

---
+++ SYBSCIT-SEM3-O2-PAYAL/partical/python programming partical/02-payaldubey/expirement no1/experiment no 2.py
@@ -0,0 +1,11 @@
+# python program to check if the input number is odd or even
+# A number is even if division by 2 given a remainder of 0.
+# If remainder is 1,it is odd number.
+
+
+num= int(input("Enter a number:"))
+if (num%2) ==0:
+    print("{0}is even".format(num))
+else:
+    print("{0}is odd".format(num))
+
